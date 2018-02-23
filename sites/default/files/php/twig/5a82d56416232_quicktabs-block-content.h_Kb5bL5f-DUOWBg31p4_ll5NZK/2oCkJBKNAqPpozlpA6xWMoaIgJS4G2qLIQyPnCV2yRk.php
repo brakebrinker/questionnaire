91bd54d427c1d78efcc6f62131415a5a77530b48
zumtum <drummen11@gmail.com>
2018-02-23 19:28:23 +0300

    make table all questionnaries

---
+++ sites/default/files/php/twig/5a82d56416232_quicktabs-block-content.h_Kb5bL5f-DUOWBg31p4_ll5NZK/2oCkJBKNAqPpozlpA6xWMoaIgJS4G2qLIQyPnCV2yRk.php
@@ -0,0 +1,87 @@
+<?php
+
+/* modules/quicktabs/templates/quicktabs-block-content.html.twig */
+class __TwigTemplate_f78e7585c59a5bad85e63c089807c423c0b7f4b023507f5f5039137c94a4f212 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 15
+        echo "<div id=\"";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["id"] ?? null), "html", null, true));
+        echo "\" class=\"";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["classes"] ?? null), "html", null, true));
+        echo "\">
+<div class=\"quicktabs-block-title\">";
+        // line 16
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["title"] ?? null), "html", null, true));
+        echo "</div>
+";
+        // line 17
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["block"] ?? null), "html", null, true));
+        echo "
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/quicktabs/templates/quicktabs-block-content.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  54 => 17,  50 => 16,  43 => 15,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "modules/quicktabs/templates/quicktabs-block-content.html.twig", "/home/ITRANSITION.CORP/m.pevnev/projects/www/questionnaire/modules/quicktabs/templates/quicktabs-block-content.html.twig");
+    }
+}
--- themes/bootstrap/js/questionaries.js
+++ themes/bootstrap/js/questionaries.js
@@ -3,9 +3,135 @@
 
     var baseUrl = location.origin;
     var format = '?_format=json';
-    var employeeQuestionaries = [];
-    var teamleadQuestionaries = [];
-    
+    var employeeQuestionaries = {};
+    var teamleadQuestionaries = {};
+    var resultQuestionaries = {};
+    var employeeResponseText;
+    var teamleadResponseText;
+    var readyTable;
+    var workTabContainer;
+    var timeQuestionarePeriod = 10800;//'15811200';
+
+    var employeeQueryUrl = '/rest/views/employee-questionnairies';
+    var teamleadQueryUrl = '/rest/views/teamlead-questionnairies';
+
+    function createTable() {
+        // var fullTable;
+
+        console.log('employeeQuestionaries');
+        console.log(employeeQuestionaries);
+
+        console.log('teamleadQuestionaries');
+        console.log(teamleadQuestionaries);
+
+        if (Object.keys(employeeQuestionaries).length > 0 && Object.keys(teamleadQuestionaries).length > 0) {
+        
+            var newTable = document.createElement('table');
+            var newTableHead = document.createElement('thead');
+            var newTableBody = document.createElement('tbody');
+            var newTableRow = document.createElement('tr');
+            var newTableHeadCell = document.createElement('th');
+            var newTableCell = document.createElement('td');
+
+            newTable.classList.add('table-striped');
+            newTableHeadCell.innerHTML = 'Новая таблица';
+            // newTableRow.insertBefore(newTableCell,newTableRow.firstChild);
+            newTableRow.insertBefore(newTableHeadCell,newTableRow.firstChild);
+            newTableHead.insertBefore(newTableRow, newTableHead.firstChild);
+            newTable.insertBefore(newTableHead, newTable.firstChild);
+            newTable.insertBefore(newTableBody, newTable.null);
+
+            // alert(employeeQuestionaries);
+
+            for (var employeeQuestionnareKey in employeeQuestionaries) {
+                resultQuestionaries.number = +employeeQuestionnareKey + 1;
+
+                for (var employeeFieldKey in employeeQuestionaries[employeeQuestionnareKey]) {
+
+                    resultQuestionaries[employeeFieldKey] = employeeQuestionaries[employeeQuestionnareKey][employeeFieldKey];
+                    // if (employeeQuestionaries[employeeQuestionnareKey].teamlead === employeeQuestionaries[employeeQuestionnareKey].author) {
+                    //     for (var teamleadQuestionareKey in teamleadQuestionaries) {
+                    //         for (var employeeFieldKey in employeeQuestionaries[employeeQuestionnareKey]) {
+                    //             resultQuestionaries[employeeFieldKey] = employeeQuestionaries[employeeQuestionnareKey][employeeFieldKey];
+                    //         }
+                    //     }
+                    // }
+
+                }
+                var teamQ = searchRowObj(employeeQuestionaries[employeeQuestionnareKey].author_id, employeeQuestionaries[employeeQuestionnareKey].created_sec);
+                console.log(teamQ);
+            }
+
+
+
+            for (var employeeQuestionnareKey in employeeQuestionaries) {
+
+                var employeeTableRow = document.createElement('tr');
+
+                newTableBody.insertBefore(employeeTableRow,null);
+
+                for (var employeeFieldKey in employeeQuestionaries[employeeQuestionnareKey]) {
+                    var employeeTableCell = document.createElement('td');
+
+                    employeeTableRow.insertBefore(employeeTableCell,null);
+
+                    switch (employeeFieldKey){
+                        default:  employeeTableCell.innerHTML = employeeQuestionaries[employeeQuestionnareKey][employeeFieldKey];
+                    }
+                }
+            }
+        }
+
+        return newTable;
+    }
+
+    function searchRowObj(employeeId, employeeDateCreated) {
+        // console.log('employeeId: ' + employeeId);
+        // console.log('employeeDateCreated: ' + employeeDateCreated);
+        
+        for (var teamleadQuestionareKey in teamleadQuestionaries) {
+            // for (var teamleadFieldKey in teamleadQuestionaries[teamleadQuestionareKey]) {
+                var timeDifference = Math.abs(teamleadQuestionaries[teamleadQuestionareKey].created_sec - employeeDateCreated);
+                
+                if (employeeId === teamleadQuestionaries[teamleadQuestionareKey].employee_id && timeDifference <= timeQuestionarePeriod) {
+                    // console.log('THIS IS KEY FOR OBJECT: ' + teamleadQuestionareKey);
+                    // console.log(teamleadQuestionaries[teamleadQuestionareKey]);
+                    
+                    return teamleadQuestionaries[teamleadQuestionareKey];
+                    break;
+                } else {
+                    console.log(employeeDateCreated);
+                    
+                }
+            // }
+        }
+
+    }
+
+    function renderTable() {
+        readyTable = createTable();
+        workTabContainer = document.getElementsByClassName('manage-questionnairies')[0];
+
+        // console.log('readyTable');
+        // console.log(readyTable);
+        window.onload = function() {
+        //     console.log('workTabContainer');
+        //     console.log(workTabContainer);
+
+
+            // if (readyTable === undefined) {
+            //     readyTable = createTable();
+            //     console.log('double TABLE');
+            //     console.log(readyTable);
+                
+            // }
+        // console.log('Object.keys(readyTable).length: ' + Object.keys(readyTable).length);
+        // console.log('Object.keys(workTabContainer).length: ' + Object.keys(workTabContainer).length);
+            if (Object.keys(workTabContainer).length > 0 && readyTable !== undefined) {
+                workTabContainer.appendChild(readyTable);
+            }
+        }
+    }
 
     function sendAjaxRequest(requestUrl) {
         var xhr = new XMLHttpRequest();
@@ -20,69 +146,48 @@
                 // return xhr.status + ': ' + xhr.statusText;
             } else {
                 // alert(xhr.responseText);
-                employeeQuestionaries = JSON.parse(xhr.responseText);
+                if (~requestUrl.indexOf(employeeQueryUrl)) {
+                    employeeResponseText = xhr.responseText;
+                    if(employeeResponseText) {
+                        try {
+                            employeeQuestionaries = JSON.parse(employeeResponseText);
+                        } catch(e) {
+                            alert(e); // error in the above string (in this case, yes)!
+                        }
+                    }
+                }
+
+                if (~requestUrl.indexOf(teamleadQueryUrl)) {
+                    teamleadResponseText = xhr.responseText;
+                    if(teamleadResponseText) {
+                        try {
+                            teamleadQuestionaries = JSON.parse(teamleadResponseText);
+                        } catch(e) {
+                            alert(e); // error in the above string (in this case, yes)!
+                        }
+                    }
+                }
+                renderTable();
             }
         }
     
         xhr.send();
     }
 
-    sendAjaxRequest(baseUrl + '/rest/views/employee-questionnairies' + format);
+    sendAjaxRequest(baseUrl + employeeQueryUrl + format);
+    sendAjaxRequest(baseUrl + teamleadQueryUrl + format);
+
 
     
-    // console.log(sendAjaxRequest(baseUrl + '/rest/views/employee-questionnairies' + format, employeeQuestionaries));
-    // teamleadQuestionaries = sendAjaxRequest(baseUrl + '/rest/views/teamlead-questionnairies' + format);
-
+    // function ready() {
+    //     // workTabContainer = document.getElementsByClassName('manage-questionnairies')[0];
+    //     var tool = document.getElementById('block-webform');
+    //     console.log(tool);
+       
+    //     alert( 'DOM готов' );
+    // }
     
-    // document.onload
-    window.onload = function(){
-        
-        console.log(employeeQuestionaries);
 
-        var workTabContainer = document.getElementsByClassName('manage-questionnairies')[0];
-        // var fullTable;
-        var newTable = document.createElement('table');
-        var newTableHead = document.createElement('thead');
-        var newTableBody = document.createElement('tbody');
-        var newTableRow = document.createElement('tr');
-        var newTableHeadCell = document.createElement('th');
-        var newTableCell = document.createElement('td');
-
-        newTable.classList.add('table-striped');
-        newTableHeadCell.innerHTML = 'Новая таблица';
-        // newTableRow.insertBefore(newTableCell,newTableRow.firstChild);
-        newTableRow.insertBefore(newTableHeadCell,newTableRow.firstChild);
-        newTableHead.insertBefore(newTableRow, newTableHead.firstChild);
-        newTable.insertBefore(newTableHead, newTable.firstChild);
-        newTable.insertBefore(newTableBody, newTable.null);
-
-        // alert(employeeQuestionaries);
-
-        for (var employeeQuestionnareKey in employeeQuestionaries) {
-
-            var employeeTableRow = document.createElement('tr');
-
-
-            newTableBody.insertBefore(employeeTableRow,null);
-
-            // document.createElement('tr').insertBefore(document.createElement('td'),null).innerHTML = key;
-            // newTableCell.innerHTML = key;
-
-            for (var employeeFieldKey in employeeQuestionaries[employeeQuestionnareKey]) {
-                var employeeTableCell = document.createElement('td');
-
-                employeeTableRow.insertBefore(employeeTableCell,null);
-
-                switch (employeeFieldKey){
-                    default:  employeeTableCell.innerHTML = employeeQuestionaries[employeeQuestionnareKey][employeeFieldKey];
-                }
-
-
-                console.log('field name: ' + employeeFieldKey + ' | field value: ' + employeeQuestionaries[employeeQuestionnareKey][employeeFieldKey]);
-            }
-        }
-
-        workTabContainer.insertBefore(newTable, workTabContainer.firstChild);
-    }
+    // document.addEventListener("DOMContentLoaded", ready);
 
 })(window.jQuery, window._);
