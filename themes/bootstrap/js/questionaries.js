14e40e17a5a245eeddd842eae260e959c283aa7b
zumtum <drummen11@gmail.com>
2018-02-26 17:58:01 +0300

    make page all questionnaries

--- themes/bootstrap/js/questionaries.js
+++ themes/bootstrap/js/questionaries.js
@@ -5,15 +5,18 @@
     var format = '?_format=json';
     var employeeQuestionaries = {};
     var teamleadQuestionaries = {};
+    var allQuestionaries = {};
     var resultQuestionaries = {};
     var employeeResponseText;
     var teamleadResponseText;
+    var allResponseText;
     var readyTable;
     var workTabContainer;
     var timeQuestionarePeriod = 10800;//'15811200';
 
     var employeeQueryUrl = '/rest/views/employee-questionnairies';
     var teamleadQueryUrl = '/rest/views/teamlead-questionnairies';
+    var allQueryUrl = '/rest/views/all-questionnairies';
 
     function createTable() {
         // var fullTable;
@@ -30,82 +33,133 @@
             var newTableHead = document.createElement('thead');
             var newTableBody = document.createElement('tbody');
             var newTableRow = document.createElement('tr');
+
             var newTableHeadCell = document.createElement('th');
+            newTableHeadCell.innerHTML = '';
+            newTableRow.insertBefore(newTableHeadCell, newTableRow.firstChild);
+            var newTableHeadCell = document.createElement('th');
+            newTableHeadCell.innerHTML = '';
+            newTableRow.insertBefore(newTableHeadCell,null);
+            var newTableHeadCell = document.createElement('th');
+            newTableHeadCell.innerHTML = '';
+            newTableRow.insertBefore(newTableHeadCell,null);
+            var newTableHeadCell = document.createElement('th');
+            newTableHeadCell.innerHTML = 'Сотрудник';
+            newTableRow.insertBefore(newTableHeadCell, null);
+            var newTableHeadCell = document.createElement('th');
+            newTableHeadCell.innerHTML = 'Дата создания';
+            newTableRow.insertBefore(newTableHeadCell, null);
+            var newTableHeadCell = document.createElement('th');
+            newTableHeadCell.innerHTML = 'Дата изменения';
+            newTableRow.insertBefore(newTableHeadCell,null);
+
+            var newTableHeadCell = document.createElement('th');
+            newTableHeadCell.innerHTML = '';
+            newTableRow.insertBefore(newTableHeadCell,null);
+            var newTableHeadCell = document.createElement('th');
+            newTableHeadCell.innerHTML = '';
+            newTableRow.insertBefore(newTableHeadCell,null);
+            var newTableHeadCell = document.createElement('th');
+            newTableHeadCell.innerHTML = '';
+            newTableRow.insertBefore(newTableHeadCell,null);
+            var newTableHeadCell = document.createElement('th');
+            newTableHeadCell.innerHTML = 'Тимлид';
+            newTableRow.insertBefore(newTableHeadCell,null);
+            var newTableHeadCell = document.createElement('th');
+            newTableHeadCell.innerHTML = 'Дата изменения';
+            newTableRow.insertBefore(newTableHeadCell,null);
+            var newTableHeadCell = document.createElement('th');
+            newTableHeadCell.innerHTML = 'Дата создания';
+            newTableRow.insertBefore(newTableHeadCell, null);
+
             var newTableCell = document.createElement('td');
 
-            newTable.classList.add('table-striped');
-            newTableHeadCell.innerHTML = 'Новая таблица';
+            newTable.classList.add('table-striped', 'table', 'table-hover');
             // newTableRow.insertBefore(newTableCell,newTableRow.firstChild);
-            newTableRow.insertBefore(newTableHeadCell,newTableRow.firstChild);
+            // newTableRow.insertBefore(newTableHeadCell,newTableRow.firstChild);
             newTableHead.insertBefore(newTableRow, newTableHead.firstChild);
             newTable.insertBefore(newTableHead, newTable.firstChild);
             newTable.insertBefore(newTableBody, newTable.null);
 
             // alert(employeeQuestionaries);
 
-            for (var employeeQuestionnareKey in employeeQuestionaries) {
-                resultQuestionaries.number = +employeeQuestionnareKey + 1;
-
-                for (var employeeFieldKey in employeeQuestionaries[employeeQuestionnareKey]) {
-
-                    resultQuestionaries[employeeFieldKey] = employeeQuestionaries[employeeQuestionnareKey][employeeFieldKey];
-                    // if (employeeQuestionaries[employeeQuestionnareKey].teamlead === employeeQuestionaries[employeeQuestionnareKey].author) {
-                    //     for (var teamleadQuestionareKey in teamleadQuestionaries) {
-                    //         for (var employeeFieldKey in employeeQuestionaries[employeeQuestionnareKey]) {
-                    //             resultQuestionaries[employeeFieldKey] = employeeQuestionaries[employeeQuestionnareKey][employeeFieldKey];
-                    //         }
-                    //     }
-                    // }
-
-                }
-                var teamQ = searchRowObj(employeeQuestionaries[employeeQuestionnareKey].author_id, employeeQuestionaries[employeeQuestionnareKey].created_sec);
-                console.log(teamQ);
+            for (var allQuestionareKey in allQuestionaries) {
+                findObjQuestionnare(allQuestionaries[allQuestionareKey].sid, allQuestionaries[allQuestionareKey].webform_id);
+                console.log('resultQuestionaries: ');
+                console.log(resultQuestionaries);
+                
             }
 
-
-
             for (var employeeQuestionnareKey in employeeQuestionaries) {
+                var teamleadSearched = searchTeamleadQuestionnare(employeeQuestionaries[employeeQuestionnareKey].author_id, employeeQuestionaries[employeeQuestionnareKey].created_sec);
+                var questionnarTableRow = document.createElement('tr');
 
-                var employeeTableRow = document.createElement('tr');
-
-                newTableBody.insertBefore(employeeTableRow,null);
+                newTableBody.insertBefore(questionnarTableRow,null);
 
                 for (var employeeFieldKey in employeeQuestionaries[employeeQuestionnareKey]) {
                     var employeeTableCell = document.createElement('td');
 
-                    employeeTableRow.insertBefore(employeeTableCell,null);
+                    // questionnarTableRow.insertBefore(employeeTableCell,null);
 
                     switch (employeeFieldKey){
+                        case 'teamlead_id': break;
+                        case 'created_sec': break;
+                        case 'author_id': break;
                         default:  employeeTableCell.innerHTML = employeeQuestionaries[employeeQuestionnareKey][employeeFieldKey];
+                        questionnarTableRow.insertBefore(employeeTableCell,null);
                     }
                 }
+
+                for (var teamleadSearchedKey in teamleadSearched) {
+                    var teamleadSearchedTableCell = document.createElement('td');
+
+                    questionnarTableRow.insertBefore(teamleadSearchedTableCell,null);
+
+                    switch (teamleadSearchedKey){
+                        case 'created_sec': break;
+                        case 'author_id': break;
+                        case 'employee_id': break;
+                        case 'locked': break;
+                        default:  teamleadSearchedTableCell.innerHTML = teamleadSearched[teamleadSearchedKey];
+                        questionnarTableRow.insertBefore(employeeTableCell,null);
+                    }
+                }
+                // console.log(teamleadSearched);
             }
         }
 
         return newTable;
     }
 
-    function searchRowObj(employeeId, employeeDateCreated) {
-        // console.log('employeeId: ' + employeeId);
-        // console.log('employeeDateCreated: ' + employeeDateCreated);
+    // search for a dependent questionnare of teamlead
+    function searchTeamleadQuestionnare(employeeId, employeeDateCreated) {
         
         for (var teamleadQuestionareKey in teamleadQuestionaries) {
-            // for (var teamleadFieldKey in teamleadQuestionaries[teamleadQuestionareKey]) {
                 var timeDifference = Math.abs(teamleadQuestionaries[teamleadQuestionareKey].created_sec - employeeDateCreated);
                 
                 if (employeeId === teamleadQuestionaries[teamleadQuestionareKey].employee_id && timeDifference <= timeQuestionarePeriod) {
-                    // console.log('THIS IS KEY FOR OBJECT: ' + teamleadQuestionareKey);
-                    // console.log(teamleadQuestionaries[teamleadQuestionareKey]);
-                    
                     return teamleadQuestionaries[teamleadQuestionareKey];
                     break;
                 } else {
-                    console.log(employeeDateCreated);
-                    
+                    // console.log(teamleadQuestionaries[teamleadQuestionareKey]);
                 }
-            // }
         }
 
+    }
+
+
+    // finds and returns a consolidated questionnaire.
+    function findObjQuestionnare(questionnareId, formType) {
+        if (formType === 'self_worth') {
+            for (employeeQuestionareKey in employeeQuestionaries) {
+                resultQuestionaries.employeeQuestionareKey = employeeQuestionaries[employeeQuestionareKey];
+                console.log(employeeQuestionaries[employeeQuestionareKey]);
+                
+            }
+        } else if (formType === 'anketa_ocenki') {
+        
+        }
+        
     }
 
     function renderTable() {
@@ -168,6 +222,18 @@
                     }
                 }
                 renderTable();
+
+                if (~requestUrl.indexOf(allQueryUrl)) {
+                    allResponseText = xhr.responseText;
+                    if(allQuestionaries) {
+                        try {
+                            teamleadQuestionaries = JSON.parse(allResponseText);
+                        } catch(e) {
+                            alert(e); // error in the above string (in this case, yes)!
+                        }
+                    }
+                }
+                renderTable();
             }
         }
     
@@ -176,6 +242,7 @@
 
     sendAjaxRequest(baseUrl + employeeQueryUrl + format);
     sendAjaxRequest(baseUrl + teamleadQueryUrl + format);
+    sendAjaxRequest(baseUrl + allQueryUrl + format);
 
 
     
