8d6fb357b3faa1931451e411e2073c5604b44182
zumtum <drummen11@gmail.com>
2018-03-07 18:10:20 +0300

    modified all questionaries views

--- modules/questionnaire_lists/src/Controller/QuestionnairesEmployeeController.php
+++ modules/questionnaire_lists/src/Controller/QuestionnairesEmployeeController.php
@@ -36,67 +36,55 @@ class QuestionnairesEmployeeController extends ControllerBase {
    * массив который спокойно пройдет через drupal_render().
    */
     public function getListForEmployee() {
+        $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+
         $output = array();
 
         $output['#title'] = 'HelloWorld page title';
 
         $output['#markup'] = 'Hello World!';
 
-        // $storage = \Drupal::entityTypeManager()->getStorage('webform_submission');
-        // $webform_submission = $storage->loadByProperties([
-        // 'entity_type' => 'node'
-        // ]);
-
-        // $submission_data = array();
-        // foreach ($webform_submission as $submission) {
-        // $submission_data[] = $submission->getData();
-
-        // }
-
-        $nids = \Drupal::entityQuery('node')->condition('type','article')->execute();
-        $nodes =  \Drupal\node\Entity\Node::loadMultiple($nids);
-
-        // $webform = \Drupal::entityTypeManager()->getStorage('webform')->load('anketa_k_dkr');
-        // $webform = $webform->getSubmissionForm();
-
-        $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
-
-        // echo 'user: ' . $user->get('uid')->value;
+        // $nids = \Drupal::entityQuery('node')->condition('type','article')->execute();
+        // $nodes =  \Drupal\node\Entity\Node::loadMultiple($nids);
 
         $webformSubmissions = \Drupal::service('entity.manager')->getStorage('webform_submission')->loadMultiple();
 
         $webformSubmissionsList = [];
         foreach ($webformSubmissions as $webformSubmission) {
+            // $webform = $webformSubmission->getWebform();
             $innerDataWebformSubmission = $webformSubmission->getData();
 
-            $webformSubmissionsList[] = array(
-                'sid' => $webformSubmission->id(),
-                'label' => $webformSubmission->label(),
-                'serial' => $webformSubmission->serial(),
-                'uuid' => $webformSubmission->uuid(),
-                // 'is_draft' => $webformSubmission->isDraft() ? t('Yes') : t('No'),
-                // 'current_page' => $webformSubmission->getCurrentPageTitle(),
-                // 'remote_addr' => $webformSubmission->getRemoteAddr(),
-                // 'submitted_by' => $webformSubmission->getOwner()->toLink(),
-                'created' => $webformSubmission->getCreatedTime(),
-                // 'completed' = WebformDateHelper::format($webform_submission->getCompletedTime()),
-                'changed' => $webformSubmission->getChangedTime(),
-                'sticky' => $webformSubmission->isSticky() ? t('Yes') : '',
-                'locked' => $webformSubmission->isLocked() ? t('Yes') : '',
-                // 'id_employee' => $innerDataWebformSubmission[''],
-                // 'notes' => $webform_submission->getNotes(),
-            );
+            // if ($submitted_by_id = $webformSubmission->getOwnerId() == $user) {
+                $webformSubmissionsList[] = array(
+                    'sid' => $webformSubmission->id(),
+                    'label' => $webformSubmission->label(),
+                    'serial' => $webformSubmission->serial(),
+                    'uuid' => $webformSubmission->uuid(),
+                    'is_draft' => $webformSubmission->isDraft() ? t('Yes') : t('No'),
+                    // 'current_page' => $webformSubmission->getCurrentPageTitle(),
+                    // 'remote_addr' => $webformSubmission->getRemoteAddr(),
+                    'submitted_by_id' => $webformSubmission->getOwnerId(),
+                    // 'completed' = WebformDateHelper::format($webform_submission->getCompletedTime()),
+                    'created' => $webformSubmission->getCreatedTime(),
+                    'changed' => $webformSubmission->getChangedTime(),
+                    'sticky' => $webformSubmission->isSticky() ? t('Yes') : '',
+                    'locked' => $webformSubmission->isLocked() ? t('Yes') : '',
+                    'id_employee' => $innerDataWebformSubmission['id_sotrudnika'],
+                    'id_teamlead' => $innerDataWebformSubmission['id_timlida'],
+                    'token' => $webformSubmission->getToken(),
+                    // 'notes' => $webform_submission->getNotes(),
+                );
+            // }
             // $contentTypesList[] = $contentType->getData();
             // $contentTypesList['teamlead_id'] = $contentType->id();
+            // echo 'webforms: ' . $webformSubmission->id();
         }
-        print_r($innerDataWebformSubmission);
-        // return $submission_data;
-
-        $testval = 'Test Value';
+        // print_r($webformSubmissionsList);
 
         return [
             '#theme' => 'questionnaires-employee-display',
             '#test_var' => $output,
+            '#submissions' => $webformSubmissionsList,
         ];
     }
 
