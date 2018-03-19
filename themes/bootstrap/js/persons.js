f6fbd7398ea3a6bcaab1b6e1d5182e840b490895
zumtum <drummen11@gmail.com>
2018-03-19 20:13:08 +0300

    finished a form validation for teamlead

--- themes/bootstrap/js/persons.js
+++ themes/bootstrap/js/persons.js
@@ -31,4 +31,24 @@
     console.log(person.name);
     person.greet();
 
+
+    var Engine = function (_model, _status) {
+        this.model = _model;
+        this.status = _status;
+    }
+
+    Engine.prototype.starting = function () {
+        console.log('Запуск двигателя.');
+        setTimeout(function() { this.status = 'working...'; alert('Двигатель ' + this.model + ' запущен') }, 1500);
+    }
+
+    Engine.prototype.stopping = function () {
+        console.log('Остановка двигателя.');
+        setTimeout(function() { this.status = 'off...'; alert('Двигатель ' + this.model +' остановлен') }, 1000);
+    }
+
+    var engine1 = new Engine('YMZ125', 'off');
+    engine1.starting();
+    engine1.stopping();
+
 })(window.jQuery, window._);
